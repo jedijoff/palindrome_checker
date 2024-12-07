f4a6a719efa3d3bf6df0a9c839d54796811ce4b5
jedijoff <jonathanwatts@mac.com>
2024-12-07 10:36:49 +0000

    Change the way the check_test var is reversed to a more Pythonic way.

--- palidrome_checker.py
+++ palidrome_checker.py
@@ -1,16 +1,14 @@
 user_text = input("Enter some text: ")
 lower_text = user_text.lower()
 check_text = []
-reverse_text = []
+
 
 for char in lower_text:
     if char != " ":
         check_text.append(char)
 
-for item in check_text:
-    reverse_text.insert(0, item)
 
-if check_text == reverse_text:
+if check_text == check_text[::-1]:
     print("It's a palindrome.")
 else:
     print("It's not a palindrome.")
